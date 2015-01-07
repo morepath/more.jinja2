2c72e909ea32afaf3bde65fe843e06dab67ff445
faassen <faassen@startifact.com>
2015-01-07 13:52:30 +0100

    test template inheritance, which exercises the loader.

---
+++ more/jinja2/tests/fixtures/template_inheritance.py
@@ -0,0 +1,16 @@
+from more.jinja2 import Jinja2App
+
+
+class App(Jinja2App):
+    pass
+
+
+@App.path(path='persons/{name}')
+class Person(object):
+    def __init__(self, name):
+        self.name = name
+
+
+@App.html(model=Person, template='templates/person2.jinja2')
+def person_default(self, request):
+    return {'name': self.name}
--- more/jinja2/tests/test_jinja2.py
+++ more/jinja2/tests/test_jinja2.py
@@ -2,7 +2,7 @@ import morepath
 import more.jinja2
 from webtest import TestApp as Client
 import pytest
-from .fixtures import template
+from .fixtures import template, template_inheritance
 
 
 def setup_module(module):
@@ -21,5 +21,25 @@ def test_template():
 <html>
 <body>
 <p>Hello world!</p>
+</body>
+</html>'''
+
+
+def test_template_inheritance():
+    config = morepath.setup()
+    config.scan(more.jinja2, ignore=['.tests'])
+    config.scan(template_inheritance)
+    config.commit()
+    c = Client(template_inheritance.App())
+
+    response = c.get('/persons/world')
+    assert response.body == b'''\
+<html>
+<head>
+</head>
+<body>
+<div id="content">
+<p>Hello world!</p>
+</div>
 </body>
 </html>'''
