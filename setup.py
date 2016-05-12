15cd6801e1d77c756c2c7e22098024944541f5a1
taschini <taschini@users.noreply.github.com>
2016-05-12 13:18:21 +0200

    Changed indentation in setup.py to placate flake8.

--- setup.py
+++ setup.py
@@ -6,27 +6,30 @@ long_description = '\n'.join((
     io.open('CHANGES.txt', encoding='utf-8').read()
 ))
 
-setup(name='more.jinja2',
-      version='0.3.dev0',
-      description="Jinja2 template integration for Morepath",
-      long_description=long_description,
-      author="Martijn Faassen",
-      author_email="faassen@startifact.com",
-      keywords='morepath jinja2',
-      license="BSD",
-      url="http://pypi.python.org/pypi/more.jinja2",
-      namespace_packages=['more'],
-      packages=find_packages(),
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
+setup(
+    name='more.jinja2',
+    version='0.3.dev0',
+    description="Jinja2 template integration for Morepath",
+    long_description=long_description,
+    author="Martijn Faassen",
+    author_email="faassen@startifact.com",
+    keywords='morepath jinja2',
+    license="BSD",
+    url="http://pypi.python.org/pypi/more.jinja2",
+    namespace_packages=['more'],
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
         'setuptools',
         'morepath >= 0.10',
-        'Jinja2 >= 2.7.3'
+        'Jinja2 >= 2.7.3',
+    ],
+    extras_require=dict(
+        test=[
+            'pytest >= 2.6.0',
+            'pytest-cov',
+            'WebTest',
         ],
-      extras_require=dict(
-        test=['pytest >= 2.6.0',
-              'pytest-cov',
-              'WebTest'],
-        ),
-      )
+    ),
+)
