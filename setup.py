146e21af28752768ec9b40d4b29047779de16580
Jürgen Gmach <juergen.gmach@googlemail.com>
2020-09-07 14:19:48 +0200

    Update supported Python versions
    modified:   .travis.yml
    modified:   CHANGES.txt
    modified:   MANIFEST.in
    modified:   setup.py
    modified:   tox.ini

--- setup.py
+++ setup.py
@@ -22,13 +22,12 @@ setup(
     zip_safe=False,
     classifiers=[
         "License :: OSI Approved :: BSD License",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
+    python_requires=">=3.6",
     install_requires=[
         'setuptools',
         'morepath >= 0.15',
